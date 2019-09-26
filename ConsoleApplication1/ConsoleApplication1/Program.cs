162360c4b6a68dfdc42301a5b685fa2378b81cd4
VoMinhHung <ng d  cht m>
2019-09-26 17:52:53 +0700

    Exercise04

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+  public  class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github");
+        }
+        public static bool IsOdd(int n)
+        {
+            if (n %2 == 1)
+                return true;
+
+            return false;
+        }
+        public static bool IsEven(int n)
+        {
+            if (n %2 == 1)
+                return false;
+            return true;
         }
     }
 }
